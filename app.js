257a17ac06c7b7d08f56e0a06b593e19c4f1fbba
luoweihua7sync <luoweihua7sync@gmail.com>
2014-11-05 12:41:12 +0800

    Update app.js
    done!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var path = require('path');
+var sep = require('path').sep;
 
 //获取当前目录下的所有文件列表数组
 function prepare(callback) {
@@ -9,7 +9,7 @@ function prepare(callback) {
             callback(files);
         } else {
             list.forEach(function (item, index) {
-                var tmp = __dirname + '/' + item;
+                var tmp = __dirname + sep + item;
                 if (fs.statSync(tmp).isDirectory()) {
                     //跳过目录
                     return;
@@ -23,32 +23,89 @@ function prepare(callback) {
 
 //根据文件名获取演唱者
 function getArtist(file, reg) {
-    var artist;
+    var artist = '';
     var match = file.match(reg);
-    var path = __dirname + '/';
     if (match) {
         artist = match[0];
     }
     return artist;
 }
 
-function classify(file) {
-    var artist = getArtist(file);
-    var dir = __dirname + "/" + artist;
-    fs.mkdir(dir, function (err) {
-        if (err && err.code !== 'EEXIST') {
-            console.log('[MKDIR_ERROR] CODE=%S', err.code);
-            return;
+function classify(file, reg, stat) {
+    var artist = getArtist(file, reg);
+    var dir = __dirname + sep + artist;
+    if (artist) {
+        fs.mkdir(dir, function (err) {
+            if (err && err.code !== 'EEXIST') {
+                stat.fail();
+                console.log('[ERROR] mkdir "%s" error', artist, err);
+                return;
+            }
+
+            var olddir = __dirname + sep + file;
+            var newdir = dir + sep + file;
+            fs.rename(olddir, newdir, function (err) {
+                if (err) {
+                    stat.fail();
+                    console.log('[ERROR] move file "%s" error', file, err);
+                    return;
+                }
+
+                stat.success();
+            });
+        });
+    } else {
+        stat.pass();
+    }
+}
+
+//统计成功失败
+function statistics(total) {
+    var _this = this;
+    var result = { total: total, success: 0, fail: 0, pass: 0 };
+    function success() {
+        result.success++;
+        isDone();
+    }
+    function fail() {
+        result.fail++;
+        isDone();
+    }
+    function pass() {
+        result.pass++;
+        isDone();
+    }
+    function done(callback) {
+        _this.callback = callback || function () { };
+    }
+    function isDone() {
+        var total = result.success + result.fail + result.pass;
+        if (total >= result.total) {
+            _this.callback(result);
         }
+    }
 
-
-    })
+    return {
+        success: success,
+        fail: fail,
+        pass: pass,
+        done: done
+    }
 }
 
 //匹配演唱者，格式必须是 [音轨.]演唱者 - 歌曲名[其他].*
 var regArtist = /(?!\s)(\w|[\u4e00-\u9fa5]|\s)*?(?=\s*[-&])/;
 prepare(function (files) {
+    var stat = new statistics(files.length);
+    stat.done(function (data) {
+        var result =
+                '\x1b[0mSuccess：\x1b[32m%s\r\n' +
+                '\x1b[0mFailure：\x1b[31m%s\r\n' +
+                '\x1b[0mPass   ：%s';
+        console.log(result, data.success, data.fail, data.pass);
+    });
+
     files.forEach(function (file) {
-        handler(file, regArtist);
-    })
-})
+        classify(file, regArtist, stat);
+    });
+});
